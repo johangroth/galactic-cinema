b3a60f1be8d81b78ac9f7235a707b4e0670bb36a
Johan Groth <johan.groth@crunch.co.uk>
2015-08-10 17:32:29 +0100

    First commit

---
+++ src/test/java/uk/org/linuxgrotto/controller/HomeControllerUnitTest.java
@@ -0,0 +1,14 @@
+package uk.org.linuxgrotto.controller;
+
+import org.junit.Test;
+
+/**
+ * Created by jgroth on 10/08/15.
+ */
+public class HomeControllerUnitTest {
+
+    @Test
+    public void testController() {
+
+    }
+}
---
+++ src/test/java/uk/org/linuxgrotto/form/MessageFormUnitTest.java
@@ -0,0 +1,14 @@
+package uk.org.linuxgrotto.form;
+
+import org.junit.Test;
+
+/**
+ * Created by jgroth on 10/08/15.
+ */
+public class MessageFormUnitTest {
+
+    @Test
+    public void testMessageForm() {
+
+    }
+}
