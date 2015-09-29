aa468c5ef39dc94540d8a957ef199f37a728b871
Johan Groth <johan.groth@crunch.co.uk>
2015-09-29 17:28:53 +0100

    Added skeleton login and signup pages and flows.

--- src/main/java/uk/org/linuxgrotto/model/Address.java
+++ src/main/java/uk/org/linuxgrotto/model/Address.java
@@ -22,5 +22,81 @@ import javax.persistence.Entity;
  * Created by jgroth on 28/09/15.
  */
 @Entity
-public class Address {
+public class Address extends GalacticCinemaEntity {
+
+    private static final long serialVersionUID = 6716634666036105590L;
+
+    private String line1;
+    private String line2;
+    private String line3;
+    private String line4;
+    private String line5;
+    private String postCode;
+    private String county;
+    private String country;
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getLine3() {
+        return line3;
+    }
+
+    public void setLine3(String line3) {
+        this.line3 = line3;
+    }
+
+    public String getLine4() {
+        return line4;
+    }
+
+    public void setLine4(String line4) {
+        this.line4 = line4;
+    }
+
+    public String getLine5() {
+        return line5;
+    }
+
+    public void setLine5(String line5) {
+        this.line5 = line5;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
 }
--- src/main/java/uk/org/linuxgrotto/model/User.java
+++ src/main/java/uk/org/linuxgrotto/model/User.java
@@ -19,6 +19,7 @@ package uk.org.linuxgrotto.model;
 import org.hibernate.validator.constraints.Email;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 /**
  * Created by jgroth on 28/09/15.
@@ -33,6 +34,31 @@ public class User extends GalacticCinemaEntity {
     @Email
     private String email;
 
+    @OneToOne
     private Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
 }
