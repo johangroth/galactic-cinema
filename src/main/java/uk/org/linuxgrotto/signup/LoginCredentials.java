fac25efd04e9a30e9ae6ee5dacc53f7014537e63
Johan Groth <johangroth1@gmail.com>
2015-12-02 06:22:22 +0000

    Added more info to signup page. Some clean up.

--- src/main/java/uk/org/linuxgrotto/signup/LoginCredentials.java
+++ src/main/java/uk/org/linuxgrotto/signup/LoginCredentials.java
@@ -27,7 +27,44 @@ public class LoginCredentials implements Serializable {
     private static final long serialVersionUID = -2560127852538433891L;
 
     private String userName;
+    private String firstName;
+    private String surname;
     private String password;
+    private String verifyPassword;
+    private String addressLine1;
+    private String addressLine2;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
 
     public String getUserName() {
         return userName;
@@ -44,4 +81,13 @@ public class LoginCredentials implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
 }
